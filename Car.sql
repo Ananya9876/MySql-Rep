1e6ee951032a1dc736101d1be10c1fad96fac624
Ananya9876 <ananyamnaik6@gmail.com>
2025-04-19 20:12:29 +0530

    creating table

---
+++ Car.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE CAR;
+USE CAR;
+CREATE TABLE DESIGNs(Wheel INT,Brand VARCHAR(30),Color VARCHAR(30),Model VARCHAR(30),Price BIGINT,Speed INT,Distance int,Milage BIGINT,Guranty INT,Insurence BIGINT);
+
+CREATE DATABASE APARTMENT;
+USE APARTMENT;
+CREATE TABLE SATVA(Area VARCHAR(30),Floor int,Flats int,Color varchar(30),Contact bigint,Register INT,Parkingarea INT,Strength BIGINT,Height INT,Width INT);
+
+CREATE DATABASE ANIMAL;
+USE ANIMAL;
+CREATE TABLE DOG(Breed VARCHAR(30),Color Varchar(30),Price Bigint,Age int,Lifespan int,Height int,Weight int,name VARCHAR(40),Food VARCHAR(30),Belt VARCHAR(30));
+
+CREATE DATABASE GADGET;
+USE GADGET;
+CREATE TABLE PHONE(Name VARCHAR(30),Brand Varchar(30),Color VARCHAR(30),Model VARCHAR(20),Year int,Gurantee int,Price BIGINT,Height int,Place varchar(30),Camerapxiel int);
+
